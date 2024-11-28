b1cd7bc4917304a49a3ac81170abeace45d400c6
CodeByRubenInamdar <rubeninamdar@gmail.com>
2024-11-28 21:03:55 +0530

    Commit

--- restaurant-management-system-1/src/main/java/com/demo/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.entity.Bill;
-
-public interface BillRepository extends JpaRepository<Bill, Long> {
-
-}
--- restaurant-management-system-1/src/main/java/com/demo/repository/DishRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.repository;
-
-import com.demo.entity.Dish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DishRepository extends JpaRepository<Dish, Long> {
-}
